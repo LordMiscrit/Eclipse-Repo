b68c332dae1964f73000df1c70f684a7c4966e65
antonio <antonio@Casa_mia>
2016-09-27 14:44:40 +0200

    prova commit

--- Prova1/src/test.java
+++ Prova1/src/test.java
@@ -2,7 +2,7 @@
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("Monteanni è stato qui");		
+		System.out.println("Monteanni è stato qui  !!");		
 		//modified
 	}
 
